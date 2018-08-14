c027bafa2f30025617c272708b7a6c4cab79da9d
Mrinmoy Das <mrinmoyfun@gmail.com>
2018-08-14 16:53:13 +0530

    Update room.js

--- room.js
+++ room.js
@@ -9,13 +9,29 @@ function run(room, player, msg)
 	// Implement your game room (server side) logic here
 	console.log("Processing " + player.name + "@" + room.name + ": " + msg);
 }
-
+var i = 10;
+var m = 1;
 function update(room)
 {
 	// Update room
 	if (room.IsPlaying())
 	{
+    if (i > 0) {
 		console.log("Room " + room.name + " is playing");
+    room.sendCommand(i);
+    i = i -1
+  } else if (i == 0) {
+    room.sendCommand("READY!");
+    i = i -1
+  } else {
+    room.sendCommand("Quiz QUESTION NO "+m);
+    m = m +1
+    i = 20
+  }
+  if (m == 10){
+    room.sendCommand("Result "+m);
+    room.Finish();
+  }
 	}
 }
 
