fbd8f62d850c38e4fd09cc11341d216c0d2b5295
Mrinmoy Das <mrinmoyfun@gmail.com>
2018-08-19 13:42:32 +0530

    Major update
    room variables are set

--- room.js
+++ room.js
@@ -3,13 +3,13 @@ Script: Node.JS Game Server - Room Logic
 Author: Huy Tran
 Email: kingbazoka@gmail.com
 */
-var json = null;
-var score1 = 0;
-var score2 = 0;
-var i = 2;
-var m = 0;
-var both = 0;
-var d = 0;
+//var json = null;
+//var score1 = 0;
+//var score2 = 0;
+//var i = 2;
+//var m = 0;
+//var both = 0;
+//var d = 0;
 
 
 function run(room, player, msg)
@@ -20,18 +20,18 @@ function run(room, player, msg)
   {
 	  var ans = msg.substring(3, msg.length - 1);
     room.broadCast('{ "code" : "OPCHOICE" , "data" : "'+ans+'" }',player);
-	var p = m-1;  
+	var p = room.m-1;  
 	  console.log("Processing " + player.name + "@" + room.name + ": " + p);
-    if (ans == json[p].correctans && player.y < m) {
+    if (ans == json[p].correctans && player.y < room.m) {
       player.x = player.x+10;
       room.sendCommand('{"code":"SCORE", "name":"'+player.name+'", "data":"'+player.x+'"}');
-      player.y = m;
-	    d = d + 1;
-    } else if (player.y < m) {
+      player.y = room.m;
+	    room.d = room.d + 1;
+    } else if (player.y < room.m) {
       player.x = player.x-5;
       room.sendCommand('{"code":"SCORE", "name":"'+player.name+'", "data":"'+player.x+'"}');
-      player.y = m;
-      d = d + 1;
+      player.y = room.m;
+      room.d = room.d + 1;
     }
 	  
    //if (room.players[0].y == p && room.players[1].y == p) {
@@ -45,12 +45,12 @@ function run(room, player, msg)
 function update(room)
 {
 	
-	  if (d > 1){
-		  i = 1;
-		  d = 0;
+	  if (room.d > 1){
+		  room.i = 1;
+		  room.d = 0;
 	  }
-  if (both <  1){
-    both = both + 1;
+  if (room.both <  1){
+    room.both = room.both + 1;
     const https = require('https');
 
     https.get('https://pg.medgag.com/quiz/api/questions.php?topic_id=2', (resp) => {
@@ -63,7 +63,7 @@ function update(room)
 
       // The whole response has been received. Print out the result.
       resp.on('end', () => {
-        console.log(JSON.parse(data).questions[1]);
+        //console.log(JSON.parse(data).questions[1]);
         json = JSON.parse(data).questions;
         var ques = JSON.stringify(json);
         //room.sendCommand('{"code":"QUESTIONS", "data":'+ques+'}');
@@ -79,22 +79,22 @@ function update(room)
 	// Update room
 	if (room.IsPlaying())
 	{
-    if (i > 0) {
+    if (room.i > 0) {
 		console.log("Room " + room.name + " is playing");
     //room.sendCommand('{"code":"COUNT", "data" : "'+i+'"}');
-    i = i -1
-  } else if (i == 0) {
+    room.i = room.i -1
+  } else if (room.i == 0) {
     room.sendCommand('{"code":"COUNT", "data" : "'+99+'"}');
-    i = i -1
+    room.i = room.i -1
   } else {
-    var t = JSON.stringify(json[m]);
-    room.sendCommand('{"code":"QUESTION", "id":'+m+', "data":'+t+'}');
-    m = m +1
-    d = 0;	  
-    i = 20
+    var t = JSON.stringify(json[room.m]);
+    room.sendCommand('{"code":"QUESTION", "id":'+room.m+', "data":'+t+'}');
+    room.m = room.m +1
+    room.d = 0;	  
+    room.i = 20
   }
 
-  if (m == 7){
+  if (room.m == 7){
     room.sendCommand('{"code":"RESULT", "data" : "result"}');
     room.players.forEach(function(c){
       // Send disconnect notify - MSG: [DC;<player name>]
@@ -103,13 +103,11 @@ function update(room)
 
     });
     room.Finish();
-    json = null;
-    score1 = 0;
-   score2 = 0;
-    i = 5;
-     m = 0;
-     both = 0;
-     d = 0;	  
+    room.json = null;
+    room.i = 2;
+     room.m = 0;
+     room.both = 0;
+     room.d = 0;	  
     //room.Finish();
   }
 	}
