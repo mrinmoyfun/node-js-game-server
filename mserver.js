c28b2cf66d134670a9e208417699173973d390c0
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-27 11:04:14 -0800

    Update mserver.js

--- mserver.js
+++ mserver.js
@@ -3,11 +3,19 @@ const http = require("http");
 const express = require("express");
 const port = process.env.PORT || 5000;
 
+import { ChatRoom } from "./chatroom";
+
 const app = express();
 app.use(express.json());
 
 const gameServer = new colyseus.Server({
   server: http.createServer(app)
+});
+// Register ChatRoom as "chat"
+gameServer.define("chat", ChatRoom);
+
+gameServer.onShutdown(function(){
+  console.log(`game server is going down.`);
 });
 
 gameServer.listen(port);
