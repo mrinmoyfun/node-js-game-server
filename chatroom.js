1140b54a469b8d753e1ce450f16e010fab400912
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-28 04:23:47 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -4,10 +4,29 @@ const colyseus = require('colyseus');
 exports.MyRoom = class extends colyseus.Room {
     // this room supports only 4 clients connected
     maxClients = 2;
+    public delayedInterval!: Delayed;
 
-    onCreate (options) {
+    // When room is initialized
+    onCreate(options) {
         console.log("BasicRoom created!", options);
+        // start the clock ticking
+        this.clock.start();
+
+        // Set an interval and store a reference to it
+        // so that we may clear it later
+        this.delayedInterval = this.clock.setInterval(() => {
+            console.log("Time now " + this.clock.currentTime);
+            this.broadcast("Time now " + this.clock.currentTime);
+        }, 1000);
+
+        // After 10 seconds clear the timeout;
+        // this will *stop and destroy* the timeout completely
+        this.clock.setTimeout(() => {
+            this.delayedInterval.clear();
+        }, 10_000);
     }
+
+  
 
     onJoin (client) {
         this.broadcast(`${ client.sessionId } joined.`);
