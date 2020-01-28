675edbaa93c4e856bb2dc06663a1d13dc9667e24
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-28 03:27:27 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -1,5 +1,6 @@
 const colyseus = require('colyseus');
 
+
 exports.MyRoom = class extends colyseus.Room {
     // this room supports only 4 clients connected
     maxClients = 2;
@@ -37,7 +38,7 @@ exports.MyRoom = class extends colyseus.Room {
 
     onMessage (client, data) {
         console.log("BasicRoom received message from", client.sessionId, ":", data);
-        this.broadcast(`(${ client.sessionId }) ${ data.message }`);
+        this.broadcast(data.message );
     }
 
     onDispose () {
