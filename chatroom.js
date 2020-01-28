056203ff84c2dc2350259bc125fdb93fadd36524
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-28 05:19:18 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -30,7 +30,7 @@ exports.MyRoom = class extends colyseus.Room {
   
 
     onJoin (room , client) {
-        this.broadcast(room);
+        this.broadcast(client.roomId);
     }
     async onLeave (client, consented) {
   // flag client as inactive for other users
