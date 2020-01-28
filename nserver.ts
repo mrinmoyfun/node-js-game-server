410e53c4ec0a79ef34ed42c69d9e622a0e71e8da
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-28 00:42:36 -0800

    Update nserver.ts

--- nserver.ts
+++ nserver.ts
@@ -9,16 +9,14 @@ import { monitor } from '@colyseus/monitor';
 // Import demo room handlers
 import { ChatRoom } from "./chatroomts";
 
-const port = Number(process.env.PORT || 5000) + Number(process.env.NODE_APP_INSTANCE || 0);
+const port = Number(process.env.PORT || 5000);
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+
 
 // Attach WebSocket Server on HTTP Server.
 const gameServer = new Server({
-  server: createServer(app),
-  express: app,
+  server: createServer(app)
 });
 
 // Register ChatRoom as "chat"
@@ -28,8 +26,7 @@ gameServer.define("chat", ChatRoom);
 
 
 
-// (optional) attach web monitoring panel
-app.use('/colyseus', monitor(gameServer));
+
 
 gameServer.onShutdown(function(){
   console.log(`game server is going down.`);
