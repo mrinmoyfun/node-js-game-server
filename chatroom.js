236205c2a8b4831662f0ad4360309fb2eda2c9e2
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-29 03:33:05 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -34,7 +34,7 @@ exports.MyRoom = class extends colyseus.Room {
     onJoin (client) {
         //this.broadcast("{ opponentId :" + this.clients[0].sessionId + "}", { except: this.clients[1] });
        // this.broadcast("{ opponentId :" + this.clients[1].sessionId + "}", { except: this.clients[0] });
-        this.boradcast(this.clients);
+        this.broadcast(this.clients);
     if (this.clients.length === 2) {
         
         let data = '';
