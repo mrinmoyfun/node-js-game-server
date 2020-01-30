dc349643a30d4f18921441aed1ee8649e2225162
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-30 07:51:31 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -15,23 +15,9 @@ class MyState extends Schema {
         super();
 
         this.players = new MapSchema();
-      
-      createPlayer (id: string) {
+    }
+  createPlayer (id) {
         this.players[ id ] = new Player();
-    }
-
-    removePlayer (id: string) {
-        delete this.players[ id ];
-    }
-
-    movePlayer (id: string, movement: any) {
-        if (movement.x) {
-            this.players[ id ].x += movement.x * 10;
-
-        } else if (movement.y) {
-            this.players[ id ].y += movement.y * 10;
-        }
-    }
     }
 }
 schema.defineTypes(MyState, {
