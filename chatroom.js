2d53040c9402ed91efb3b2769f44ce826861c92f
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-30 07:45:07 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -1,5 +1,25 @@
 const colyseus = require('colyseus');
-import { Schema, type, MapSchema } from "@colyseus/schema";
+const schema = require('@colyseus/schema');
+const Schema = schema.Schema;
+const MapSchema = schema.MapSchema;
+
+class Player extends Schema {
+}
+schema.defineTypes(Player, {
+  x: "number",
+  y: "number"
+});
+
+class MyState extends Schema {
+    constructor () {
+        super();
+
+        this.players = new MapSchema();
+    }
+}
+schema.defineTypes(MyState, {
+  players: { map: Player }
+});
 
 
 
