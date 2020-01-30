ec3287dd9f84b8902d9c7518caa8d1b59d9fa9e7
Mrinmoy Das <mrinmoyfun@gmail.com>
2020-01-30 09:13:33 -0800

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -118,8 +118,8 @@ req.end()
         }
         var ques = {q: ff.questions[this.state.qid] , qid:this.state.qid, empty: true};
       
-        this.state.players[this.clients[0].sessionId].y = 0;
-        this.state.players[this.clients[1].sessionId].y = 0;
+        //this.state.players[this.clients[0].sessionId].y = 0;
+        //this.state.players[this.clients[1].sessionId].y = 0;
         this.broadcast(ques);
        // this.state.q = ff.questions[this.state.qid];
         this.state.qid++;
